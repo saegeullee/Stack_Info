ba6c29f5bb055e9542561f7c77781b66a171965c
saegeullee <lemix777@naver.com>
2021-09-26 01:17:45 +0900

    add validation pipeline

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //whitelist: true -> automatically removes random property on post request body
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
 bootstrap();
