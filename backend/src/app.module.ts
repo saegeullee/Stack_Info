78fff646c2300e4ca4d9bdec53a740a64b16ed94
saegeullee <lemix777@naver.com>
2021-09-26 00:22:40 +0900

    connect database and registered companyField entity on typeorm module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompanyModule } from './company/company.module';
 import { StackModule } from './stack/stack.module';
+import { CompanyField } from './company/company-field/company-field.entity';
 
 @Module({
-  imports: [CompanyModule, StackModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [CompanyField],
+      synchronize: true,
+    }),
+    CompanyModule,
+    StackModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
