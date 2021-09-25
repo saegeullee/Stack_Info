214845341b39d04efdf389cb5f25ea969e8b671c
saegeullee <lemix777@naver.com>
2021-09-26 01:18:45 +0900

    add company field service, controller and dto

--- backend/src/company/company-field/company-field.controller.ts
+++ backend/src/company/company-field/company-field.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CompanyFieldService } from './company-field.service';
+import { CreateCompanyFieldDto } from './dtos/create-company-field.dto';
 
 @Controller('company-field')
-export class CompanyFieldController {}
+export class CompanyFieldController {
+  constructor(private companyFieldService: CompanyFieldService) {}
+
+  @Get()
+  async findCompayFields() {
+    return await this.companyFieldService.find();
+  }
+
+  @Post()
+  async createCompanyField(@Body() body: CreateCompanyFieldDto) {
+    await this.companyFieldService.create(body.name);
+  }
+
+  @Get('/:id')
+  async findCompanyField(@Param('id') id: string) {
+    return await this.companyFieldService.findOne(parseInt(id));
+  }
+}
--- backend/src/company/company-field/company-field.service.ts
+++ backend/src/company/company-field/company-field.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CompanyField } from './company-field.entity';
 
 @Injectable()
-export class CompanyFieldService {}
+export class CompanyFieldService {
+  constructor(
+    @InjectRepository(CompanyField) private repo: Repository<CompanyField>,
+  ) {}
+
+  create(name: string) {
+    const companyField = this.repo.create({ name });
+    return this.repo.save(companyField);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOne(id);
+  }
+
+  find() {
+    return this.repo.find();
+  }
+}
